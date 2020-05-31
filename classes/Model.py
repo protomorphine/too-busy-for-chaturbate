f421f6706deceade7ea9c336148c4ddfbe2c2a76
Daniil Zaitsev <kupernewfag@gmail.com>
2020-05-31 14:21:05 +0300

    123

--- classes/Model.py
+++ classes/Model.py
@@ -1,14 +1,16 @@
 import os
 import time
 import selenium
+import requests
 from seleniumwire import webdriver
 
-
+# TODO: don't use selenium cause requests faster
 class Model(object):
     def __init__(self, nickname):
         super(Model, self).__init__()
         self.nickname = nickname
         self.chaturbate_link = "https://chaturbate.eu/" + self.nickname
+        self.json_data = {}
         self.online = False
         self.driver = 0
         self.m3u8_link = ""
@@ -17,29 +19,11 @@ class Model(object):
     # |    Инициализая подключения к chaturbate.eu + подтверждение возраста    |
     # +------------------------------------------------------------------------+
     def connect_to_chaturbate(self):
-        # запускаем firefox с параметром headless
-        # чтобы не создавать окно браузера
-        options = selenium.webdriver.firefox.options.Options()
-        options.headless = True
-        # настройки proxy
-        ip, port = "192.168.1.1", "443"
-        firefox_profile = webdriver.FirefoxProfile()
-        firefox_profile.set_preference("network.proxy.type", 1)
-        firefox_profile.set_preference("network.proxy.socks", ip)
-        firefox_profile.set_preference("network.proxy.socks_port", int(port))
-
-        self.driver = webdriver.Firefox(firefox_profile, options=options)
-
         print("Trying to connect " + self.chaturbate_link + " ...")
-        self.driver.get(self.chaturbate_link)
-
-        try:
-            entrance_btn = self.driver.find_element_by_id("close_entrance_terms")
-        except selenium.common.exceptions.NoSuchElementException:
-            return False
-
-        entrance_btn.click()
-        return self.driver
+        self.json_data = requests.get(
+            "https://chaturbate.eu/api/chatvideocontext/{}".format(model.nickname)
+        ).json()
+        return self.json_data
 
     def close_connection(self):
         try:
@@ -52,19 +36,8 @@ class Model(object):
     # |         Возвращает состояние трансляции модели (Online/Offline)        |
     # +------------------------------------------------------------------------+
     def is_online(self):
-        if not self.driver:
-            return -1
-        # на chaturbate.eu чтобы узнать онлайн модель или нет необходимо
-        # найти на странице тэг <video> и узнать параметр src
-        # если src есть - трансляция запущена
-        video_player = self.driver.find_element_by_tag_name("video")
-        video_src = video_player.get_attribute("src")
-
-        if not video_src:
-            return False
-        self.online = True
-        print(self.nickname + " is online now")
-        return True
+        if json_data["room_status"] not "offline":
+            print("Room is currently offline")
 
     # +------------------------------------------------------------------------+
     # |                 Вытягивает ссылку на потокое видео                     |
--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from classes.Vidak import Vidak
 
 def main():
     try:
-        ffmpeg_path = "C:\\Program Files\\ffmpeg\\bin\\ffmpeg.exe"
-        vids_folder = "D:\\vids_chaturbate\\"
+        FFMPEG_PATH = "C:\\Program Files\\ffmpeg\\bin\\ffmpeg.exe"
+        VIDS_PATHH = "D:\\vids_chaturbate\\"
 
         welcome_logo = open("text\\wellcome.txt", "r").read()
         subprocess.call("cls", shell=True)
@@ -15,7 +15,7 @@ def main():
         model = Model(input("Please, enter model nickname:\n> "))
         if model.connect_to_chaturbate():
             model.get_m3u8_link()
-            vidak = Vidak(ffmpeg_path, vids_folder, model)
+            vidak = Vidak(FFMPEG_PATH, VIDS_PATHH, model)
             vidak.record_m3u8_stream()
         else:
             print(
@@ -24,7 +24,6 @@ def main():
                 + " doesn't exist.\n"
                 + "Please check nickname and try again."
             )
-
     except KeyboardInterrupt:
         print("Script was stopped by user. Exiting.")
 
