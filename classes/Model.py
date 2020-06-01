c65ed42d01bdff1a049e939318bf92e4d326c86a
Daniil Zaitsev <kupernewfag@gmail.com>
2020-06-01 23:32:33 +0300

    added posibily to record broad cast in backgroung

--- classes/Model.py
+++ classes/Model.py
@@ -5,35 +5,31 @@ class Model(object):
     def __init__(self, nickname):
         super(Model, self).__init__()
         self.nickname = nickname
-        self.chaturbate_link = "https://chaturbate.eu/" + self.nickname
+        self.chaturbate_link = f"https://chaturbate.eu/{self.nickname}"
         self.json_data = self.connect_to_chaturbate()
         self.online = False
         self.m3u8_link = self.get_m3u8_link()
 
     def connect_to_chaturbate(self):
-        print("Trying to connect {} ... ".format(self.chaturbate_link), end="")
+        print(f"Trying to connect {self.chaturbate_link} ... ", end="")
         try:
             json_data = requests.get(
-                "https://chaturbate.eu/api/chatvideocontext/{}".format(self.nickname)
+                f"https://chaturbate.eu/api/chatvideocontext/{self.nickname}"
             ).json()
             print("success")
         except ValueError:
             print("failed")
-            print("HTTP 404 - Model {} not found!".format(self.nickname))
+            print(f"HTTP 404 - Model {self.nickname} not found!")
             raise SystemExit(-1)
         return json_data
 
     def is_online(self):
         if self.json_data["room_status"] not in ["offline", "private"]:
             self.online = True
-        print("Room status is {}".format(self.json_data["room_status"]))
+        print(f"Room status is {self.json_data['room_status']}")
         if self.json_data["room_status"] == "private":
             print(
-                "You can spy {nickname} show for {price} tkn per minute on {link}".format(
-                    nickname=self.nickname,
-                    price=self.json_data["spy_private_show_price"],
-                    link=self.chaturbate_link,
-                )
+                f"You can spy {self.nickname} show for {self.json_data['spy_private_show_price']} tkn per minute on {self.chaturbate_link}"
             )
         return self.online
 
--- classes/Vidak.py
+++ classes/Vidak.py
@@ -9,49 +9,58 @@ class Vidak(object):
         self.fmmpeg_path = ffmpeg_path
         self.model = model
         self.workind_dir = vids_folder + model.nickname
-        self.log_file = "{}\\ffmpeg.log".format(self.workind_dir)
+        self.log_file = f"{self.workind_dir}\\ffmpeg.log"
 
     def output_name(self):
         time_now = time.strftime("%d-%b-%H-%M-%S-")
-        return "{time}{nickname}-chaturbate.mkv".format(
-            time=time_now, nickname=self.model.nickname
-        )
+        return f"{time_now}{self.model.nickname}-chaturbate.mkv"
 
     def record_m3u8_stream(self):
         if self.model.online:
+            background_rec = input(
+                "Do u want to record broadcast in background? (y/n) "
+            ).lower()
             approve_record = input("Start recording broadcast? (y/n) ").lower()
             if approve_record in ["y", "yes", ""]:
                 if not os.path.exists(self.workind_dir):
                     os.mkdir(self.workind_dir)
                 out_file = self.output_name()
-                full_path = self.workind_dir + "\\" + out_file
+                full_path = f"{self.workind_dir}\\{out_file}"
+                ffmpeg_exec = [
+                    self.fmmpeg_path,
+                    "-i",
+                    self.model.m3u8_link,
+                    "-c",
+                    "copy",
+                    "-vcodec",
+                    "copy",
+                    "-bsf:a",
+                    "aac_adtstoasc",
+                    full_path,
+                ]
                 print(
-                    "Recording broadcast in {}".format(full_path)
+                    f"Recording broadcast in {full_path}"
                     + "\nPlease don't close this window\n"
                     + "To stop  recording press CTRL + C"
                 )
                 try:
                     with open(self.log_file, "w") as ffmpeg_log:
-                        subprocess.call(
-                            [
-                                self.fmmpeg_path,
-                                "-i",
-                                self.model.m3u8_link,
-                                "-c",
-                                "copy",
-                                "-vcodec",
-                                "copy",
-                                "-bsf:a",
-                                "aac_adtstoasc",
-                                full_path,
-                            ],
-                            stdout=ffmpeg_log,
-                            stderr=subprocess.STDOUT,
-                        )
+                        if background_rec in ["y", "yes", ""]:
+                            subprocess.Popen(
+                                ffmpeg_exec,
+                                stdout=ffmpeg_log,
+                                stderr=subprocess.STDOUT,
+                            )
+                        else:
+                            subprocess.call(
+                                ffmpeg_exec,
+                                stdout=ffmpeg_log,
+                                stderr=subprocess.STDOUT,
+                            )
                 except KeyboardInterrupt:
                     print(
                         "Recording was stopped by user.\n"
-                        + "Recorded broadcast in {}".format(full_path)
+                        + f"Recorded broadcast in {full_path}"
                     )
-                return "Recorded in " + full_path
+                return f"Recorded in {full_path}"
             raise SystemExit(-1)
